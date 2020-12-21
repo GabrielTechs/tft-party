ee445ec47f96faeb6ad42e77e168c98629fdd8de
LAPTOP-K6QB57M2\gabha <gabha2107@gmail.com>
2020-12-21 11:46:06 -0400

    toolbar added to app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
 
+import Toolbar from "./components/Toolbar";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Toolbar></Toolbar>
+    </div>
+  );
 }
 
 export default App;
