f7290a8a09f486c2b6a85575fabf5580f03f9403
LAPTOP-K6QB57M2\gabha <gabha2107@gmail.com>
2020-12-22 12:22:21 -0400

    font awesome project library added

---
+++ src/assets/FontAwesomeIcon.js
@@ -0,0 +1,11 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  fab,
+  faFacebookSquare,
+  faInstagram,
+  faWhatsapp,
+} from "@fortawesome/free-brands-svg-icons";
+
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+
+library.add(fab, faFacebookSquare, faHome, faInstagram, faWhatsapp);
