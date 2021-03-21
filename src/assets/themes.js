3cf8ef1c9345fc3431ba7302b9176ab8a8ab10fc
LAPTOP-K6QB57M2\gabha <gabha2107@gmail.com>
2021-03-21 13:31:31 -0400

    file for all posible themes variables

---
+++ src/assets/themes.js
@@ -0,0 +1,28 @@
+const lightTheme = {
+  bg: "rgb(242, 253, 255)",
+  primary: "rgb(2, 76, 140)",
+  secondary: "rgb(4, 117, 216)",
+  tertiary: "rgb(92, 124, 165)",
+  warning: "rgb(219, 10, 0)",
+  success: "rgb(53, 219, 0)",
+  primaryText: "rgb(7, 6, 0)",
+  secondaryText: "rgb(250, 255, 253)",
+};
+
+const darkTheme = {
+  bg: "rgb(10, 9, 8)",
+  primary: "rgb(242, 4, 91)",
+  secondary: "rgb(242, 4, 115)",
+  tertiary: "rgb(92, 124, 165)",
+  warning: "rgb(219, 15, 7)",
+  success: "rgb(5, 242, 23)",
+  primaryText: "rgb(250, 255, 253)",
+  secondaryText: "rgb(7, 6, 0)",
+};
+
+const themes = {
+  light: lightTheme,
+  dark: darkTheme,
+};
+
+export { themes, lightTheme, darkTheme };
