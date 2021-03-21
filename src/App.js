14e6481b9e1d66cdc5c8b39b0e17ecf48209190d
LAPTOP-K6QB57M2\gabha <gabha2107@gmail.com>
2021-03-21 13:29:47 -0400

    added the themeProvider and theme logic with toggler and icon

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
 import "./App.css";
+import "./assets/FontAwesomeIcon";
 
-import NavBar from "./components/Nav/NavBar";
+import { ThemeProvider } from "styled-components";
+import useTheme from "./assets/useTheme";
+import GlobalStyles from "./assets/GlobalStyles";
+
+import NavBar from "./components/NavBar";
+import { themes } from "./assets/themes";
 
 function App() {
+  const { theme, themeToggler, icon } = useTheme();
+
   return (
-    <div className="App">
-      <NavBar />
-    </div>
+    <ThemeProvider theme={themes[theme]}>
+      <GlobalStyles />
+      <div className="App">
+        <NavBar theme={theme} themeToggler={themeToggler} icon={icon} />
+        <h1>Hello World</h1>
+      </div>
+    </ThemeProvider>
   );
 }
 
