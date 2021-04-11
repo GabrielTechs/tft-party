816afc3fa241a512d6be2ca2fd2691a719784c3c
LAPTOP-K6QB57M2\gabha <gabha2107@gmail.com>
2021-04-11 16:21:29 -0400

    adding responsiveness

--- src/components/ModeHandler.js
+++ src/components/ModeHandler.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 
+import { mediaQueries } from "../assets/mediaQueries";
+
 const ModeHandler = () => {
   return (
     <ModeHandlerDiv>
@@ -12,6 +14,9 @@ const ModeHandler = () => {
 
 const ModeHandlerDiv = styled.div`
   padding: 69px;
+  ${mediaQueries("md")`
+  padding: 26px;
+  `}
 `;
 
 const RandomModeDiv = styled.div`
