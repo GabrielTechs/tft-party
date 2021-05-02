f9968e4399ef6fe75fede8798d231848824b7328
LAPTOP-K6QB57M2\gabha <gabha2107@gmail.com>
2021-05-02 19:29:18 -0400

    adding the special rule text

--- src/components/ThemeToggleBtn.js
+++ src/components/ThemeToggleBtn.js
@@ -1,10 +1,30 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styled from "styled-components";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const ThemeToggleBtn = (props) => {
-  return <ThemeBtn onClick={props.themeToggler}>theme toggler</ThemeBtn>;
+  return (
+    <ThemeBtn onClick={props.themeToggler}>
+      <FontAwesomeIcon icon={props.icon} size="1x" />
+    </ThemeBtn>
+  );
 };
 
-const ThemeBtn = styled.button``;
+const ThemeBtn = styled.button`
+  background: none;
+  border: none;
+  cursor: pointer;
+  color: ${({ theme }) => theme.secondaryText};
+  font-size: 2rem;
+  margin-left: 15px;
+  transition: 0.8s;
+`;
+
+ThemeToggleBtn.propTypes = {
+  themeToggler: PropTypes.func,
+  icon: PropTypes.array,
+};
 
 export default ThemeToggleBtn;
