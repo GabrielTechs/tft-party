c63c7d72230171d9c768aeb030d7640975d3c335
LAPTOP-K6QB57M2\gabha <gabha2107@gmail.com>
2021-05-16 14:18:50 -0400

    adding RulesPlaceHolders to have data to display

---
+++ src/assets/RulesPlaceHolders.js
@@ -0,0 +1,85 @@
+export const Modes = {
+  yinYang: {
+    rules: [
+      "1. You have to buy every king you see.",
+      "2. You can't sell the kings.",
+      "3. The kings must be on the field.",
+    ],
+    modeDescription:
+      "Sometimes the light and the shadow must fight together, you will be given a chosen one from each side to fight with you, so you must find a way to balance the power. The player who balance the yin and yang will be victorious.",
+    modeName: "Yin-Yang",
+  },
+  modeTwo: {
+    rules: [
+      "1. Rule number one.",
+      "2. Rule number two",
+      "3. Rule number three",
+    ],
+    modeDescription: "In this mode you will be given...",
+    modeName: "Mode two",
+  },
+  modeThree: {
+    rules: [
+      "1. Rule number one.",
+      "2. Rule number two",
+      "3. Rule number three",
+    ],
+    modeDescription: "In this mode you will be given...",
+    modeName: "Mode three",
+  },
+  modeFour: {
+    rules: [
+      "1. Rule number one.",
+      "2. Rule number two",
+      "3. Rule number three",
+    ],
+    modeDescription: "In this mode you will be given...",
+    modeName: "Mode four",
+  },
+  modeFive: {
+    rules: [
+      "1. Rule number one.",
+      "2. Rule number two",
+      "3. Rule number three",
+    ],
+    modeDescription: "In this mode you will be given...",
+    modeName: "Mode five",
+  },
+  modeSix: {
+    rules: [
+      "1. Rule number one.",
+      "2. Rule number two",
+      "3. Rule number three",
+    ],
+    modeDescription: "In this mode you will be given...",
+    modeName: "Mode six",
+  },
+  modeSeven: {
+    rules: [
+      "1. Rule number one.",
+      "2. Rule number two",
+      "3. Rule number three",
+    ],
+    modeDescription: "In this mode you will be given...",
+    modeName: "Mode seven",
+  },
+};
+
+export const SRules = {
+  specialRuleOne: {
+    specialRule: "You don't have to buy the chosen one after is 2 star",
+    specialRuleName: "Chosen one 2 star",
+  },
+  specialRuleTwo: {
+    specialRule: "Special rule two",
+    specialRuleName: "Special rule two",
+  },
+  specialRuleThree: {
+    specialRule: "Special rule three",
+    specialRuleName: "Special rule three",
+  },
+  specialRuleFour: {
+    specialRule: "Special rule four",
+    specialRuleName: "Special rule four",
+  },
+};
