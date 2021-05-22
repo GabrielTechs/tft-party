a79c5f4b6746e8451051dd3796203f142a5d76aa
LAPTOP-K6QB57M2\gabha <gabha2107@gmail.com>
2021-05-22 01:17:38 -0400

    added a new condition to use backgraund color with icon

--- src/components/SHexagon.js
+++ src/components/SHexagon.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+
 import { useContext } from "react";
 import { ThemeContext } from "styled-components";
 
@@ -8,25 +9,37 @@ import Hexagon from "react-hexagon";
 
 const SHexagon = (props) => {
   const themeContext = useContext(ThemeContext);
-  const img = props.img;
 
   return (
     <HexagonDiv>
-      {img ? (
+      {props.img ? (
         <Hexagon
           style={{
             stroke: `${themeContext.primary}`,
-            strokeWidth: 30,
+            strokeWidth: 20,
           }}
           backgroundImage={props.img}
           backgroundScale={1.001}
         />
+      ) : props.background ? (
+        <Hexagon
+          className="umbrella"
+          style={{
+            stroke: `${themeContext.primary}`,
+            strokeWidth: 30,
+            fill: `${themeContext.primary}`,
+          }}
+          backgroundScale={1.001}
+        >
+          {props.icon && (
+            <image href={props.icon} x="16%" y="16%" height="69%" width="69%" />
+          )}
+        </Hexagon>
       ) : (
         <Hexagon
           style={{
             stroke: `${themeContext.primary}`,
             strokeWidth: 30,
-            fill: `${props.background}`,
           }}
           backgroundScale={1.001}
         />
@@ -37,11 +50,14 @@ const SHexagon = (props) => {
 
 SHexagon.propTypes = {
   img: PropTypes.string,
-  background: PropTypes.string,
+  background: PropTypes.bool,
+  icon: PropTypes.string,
+  stroke: PropTypes.string,
 };
 
 const HexagonDiv = styled.div`
-  height: 100%;
+  max-height: 100%;
+  max-width: 100%;
   padding: 0.69px;
   svg {
     height: 100%;
