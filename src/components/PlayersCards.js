861e7c4889646027995b2b5464d3f3990988a330
LAPTOP-K6QB57M2\gabha <gabha2107@gmail.com>
2021-05-29 18:15:57 -0400

    initializing PlayersCards component

---
+++ src/components/PlayersCards.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const PlayersCards = () => {
+  return (
+    <PlayersCardsDiv>
+      <h1>Players names</h1>
+      <h1>Players setup</h1>
+    </PlayersCardsDiv>
+  );
+};
+
+const PlayersCardsDiv = styled.div``;
+
+export default PlayersCards;
