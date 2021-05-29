66ca456ba8dc8e0a45fa1a4cb599b3ae2e034251
LAPTOP-K6QB57M2\gabha <gabha2107@gmail.com>
2021-05-29 18:16:27 -0400

    styling div and headers

--- src/components/PlayersCards.js
+++ src/components/PlayersCards.js
@@ -10,6 +10,23 @@ const PlayersCards = () => {
   );
 };
 
-const PlayersCardsDiv = styled.div``;
+const PlayersCardsDiv = styled.div`
+  display: flex;
+  flex-grow: 1;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  border: 3px solid ${({ theme }) => theme.primary};
+  border-radius: 15px;
+  margin: 69px;
+  h1 {
+    color: ${({ theme }) => theme.primaryText};
+    display: inline-block;
+    font-size: 2.69rem;
+    font-weight: 500;
+    margin: 9px;
+    text-align: center;
+    width: 100%;
+  }
+`;
 
 export default PlayersCards;
