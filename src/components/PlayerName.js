ad204545d3c4306097e06e40ee9f7153a1b284d2
LAPTOP-K6QB57M2\gabha <gabha2107@gmail.com>
2021-05-31 11:47:56 -0400

    binding the input and displaying player name correctly

--- src/components/PlayerName.js
+++ src/components/PlayerName.js
@@ -5,7 +5,13 @@ import styled from "styled-components";
 const PlayerName = (props) => {
   return (
     <PlayerNameLabel>
-      <PlayerNameInput />
+      {props.player}
+      <PlayerNameInput
+        name="player"
+        type="text"
+        placeholder={props.player}
+        {...props.playerBind}
+      />
     </PlayerNameLabel>
   );
 };
@@ -14,6 +20,10 @@ const PlayerNameLabel = styled.label``;
 
 const PlayerNameInput = styled.input``;
 
-PlayerName.propTypes = {};
+PlayerName.propTypes = {
+  player: PropTypes.string,
+  playerName: PropTypes.string,
+  playerBind: PropTypes.object,
+};
 
 export default PlayerName;
