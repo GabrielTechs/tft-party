75b776a32ad6c6504bd7522c1503b921de93136c
LAPTOP-K6QB57M2\gabha <gabha2107@gmail.com>
2021-05-31 11:43:52 -0400

    getting playersInput from usePlayers and passing it as props to PlayersNames

--- src/components/PlayersCards.js
+++ src/components/PlayersCards.js
@@ -2,13 +2,18 @@ import React from "react";
 import styled from "styled-components";
 import { mediaQueries } from "../assets/mediaQueries";
 
+import usePlayers from "../hooks/usePlayers";
 import PlayersNames from "./PlayersNames";
+import RerollPlayersSetup from "./RerollPlayersSetup";
 
 const PlayersCards = () => {
+  const { playersInputs } = usePlayers();
+
   return (
     <PlayersCardsDiv>
       <h1>Players names</h1>
-      <PlayersNames players="players" />
+      <PlayersNames players={playersInputs} />
+      <RerollPlayersSetup />
       <h1>Players setup</h1>
     </PlayersCardsDiv>
   );
