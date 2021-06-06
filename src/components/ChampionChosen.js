eb46ea016f2d92ff13513067d9af94e8b674a6d2
LAPTOP-K6QB57M2\gabha <gabha2107@gmail.com>
2021-06-06 18:17:27 -0400

    adding SHexagon for champion imges

--- src/components/ChampionChosen.js
+++ src/components/ChampionChosen.js
@@ -2,10 +2,15 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
+import SHexagon from "./SHexagon";
+
 const ChampionChosen = (props) => {
   return (
     <ChampionChosenDiv>
-      <ChampionChosenImgDiv></ChampionChosenImgDiv>
+      <ChampionChosenImgDiv>
+        <SHexagon background={true} />
+      </ChampionChosenImgDiv>
+      <h4>Hi</h4>
     </ChampionChosenDiv>
   );
 };
