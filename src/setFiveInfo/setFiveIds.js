6f92fbdee0b68213486de44bb56ebe3a9603b43b
LAPTOP-K6QB57M2\gabha <gabha2107@gmail.com>
2021-06-20 14:54:00 -0400

    origins divided in light and shadow with new const each

--- src/setFiveInfo/setFiveIds.js
+++ src/setFiveInfo/setFiveIds.js
@@ -139,3 +139,23 @@ export const setFiveOriginsId = [
   "Set5_Nightbringer",
   "Set5_Revenant",
 ];
+
+export const setFiveShadowOriginsId = [
+  "Set5_Eternal",
+  "Set5_Abomination",
+  "Set5_Coven",
+  "Set5_Dragonslayer",
+  "Set5_Forgotten",
+  "Set5_Hellion",
+  "Set5_Nightbringer",
+  "Set5_Revenant",
+];
+
+export const setFiveLightOriginsId = [
+  "Set5_Redeemed",
+  "Set5_Dawnbringer",
+  "Set5_Verdant",
+  "Set5_Draconic",
+  "Set5_Ironclad",
+  "Set5_Eternal",
+];
