db67b60961046a936b962839b4a0333494a54d0d
LAPTOP-K6QB57M2\gabha <gabha2107@gmail.com>
2021-06-20 13:26:10 -0400

    listening to firestore collection with snapshot

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -1,5 +1,22 @@
-const useFirestore = () => {
-  return {};
+import { useState, useEffect } from "react";
+import { projectFirestore } from "../firebase/config";
+
+const useFirestore = (collection) => {
+  const [docsCategory, setDocsCategory] = useState([]);
+
+  useEffect(() => {
+    const collectionRef = projectFirestore.collection(collection);
+    const unsub = collectionRef.onSnapshot((snap) => {
+      let categoryDocuments = [];
+      snap.forEach((doc) => {
+        categoryDocuments.push({ ...doc.data(), id: doc.id });
+      });
+      setDocsCategory(categoryDocuments);
+    });
+    return () => unsub();
+  }, [collection]);
+
+  return { docsCategory };
 };
 
 export default useFirestore;
