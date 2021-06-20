0a03a3790a18004f07d8e6e3db91823b3ceda332
LAPTOP-K6QB57M2\gabha <gabha2107@gmail.com>
2021-06-20 13:27:47 -0400

    conditioning the function useFirestore to accept a "where" firestore query

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -1,20 +1,37 @@
 import { useState, useEffect } from "react";
 import { projectFirestore } from "../firebase/config";
 
-const useFirestore = (collection) => {
+const useFirestore = (collection, category, categoryName) => {
   const [docsCategory, setDocsCategory] = useState([]);
 
   useEffect(() => {
     const collectionRef = projectFirestore.collection(collection);
-    const unsub = collectionRef.onSnapshot((snap) => {
-      let categoryDocuments = [];
-      snap.forEach((doc) => {
-        categoryDocuments.push({ ...doc.data(), id: doc.id });
+
+    // Listening to the collection filtered by category.
+    if (category !== "all") {
+      const unsub = collectionRef
+        .where(category, "==", categoryName)
+        .onSnapshot((snap) => {
+          let categoryDocuments = [];
+          snap.forEach((doc) => {
+            categoryDocuments.push({ ...doc.data(), id: doc.id });
+          });
+          setDocsCategory(categoryDocuments);
+        });
+      return () => unsub();
+    }
+    // Listening to the entire collection.
+    else {
+      const unsub = collectionRef.onSnapshot((snap) => {
+        let categoryDocuments = [];
+        snap.forEach((doc) => {
+          categoryDocuments.push({ ...doc.data(), id: doc.id });
+        });
+        setDocsCategory(categoryDocuments);
       });
-      setDocsCategory(categoryDocuments);
-    });
-    return () => unsub();
-  }, [collection]);
+      return () => unsub();
+    }
+  }, [collection, category, categoryName]);
 
   return { docsCategory };
 };
