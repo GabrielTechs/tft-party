bbada0249314b1f43150624403699ae689338a3b
LAPTOP-K6QB57M2\gabha <gabha2107@gmail.com>
2021-07-02 23:37:55 -0400

    getting modes from firebase

--- src/hooks/useRules.js
+++ src/hooks/useRules.js
@@ -1,5 +1,28 @@
+import { useState, useEffect } from "react";
+
+import { projectFirestore } from "../firebase/config";
+
 const useRules = () => {
-  return {};
+  const [modes, setModes] = useState([]);
+
+  useEffect(() => {
+    const collectionModesRef = projectFirestore.collection("rules");
+
+    const unsub = collectionModesRef.onSnapshot((snap) => {
+      snap.forEach((doc) => {
+        if (doc.data().name === "modes") {
+          setModes(doc.data().modes);
+        }
+      });
+    });
+    return () => {
+      unsub();
+    };
+  }, []);
+
+  return {
+    modes,
+  };
 };
 
 export default useRules;
