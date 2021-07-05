706cbeb37b3f7a8c82ba94f9320cdaff418687db
LAPTOP-K6QB57M2\gabha <gabha2107@gmail.com>
2021-07-04 23:29:52 -0400

    importing all the components needed

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,19 @@
 import React, { Fragment } from "react";
 
+import ModeHandler from "../components/ModeHandler";
+import SpecialRules from "../components/SpecialRules";
+import RulesActive from "../components/RulesActive";
+import PlayersCards from "../components/PlayersCards";
+
 const HomePage = () => {
-  return <Fragment></Fragment>;
+  return (
+    <Fragment>
+      <ModeHandler />
+      <SpecialRules />
+      <RulesActive />
+      <PlayersCards />
+    </Fragment>
+  );
 };
 
 export default HomePage;
