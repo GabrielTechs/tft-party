0caf4e2ccdb88a203f0739b9e3abc6794189b995
LAPTOP-K6QB57M2\gabha <gabha2107@gmail.com>
2021-07-09 15:51:34 -0400

    function to get random commanders from the setFiveChampions array

--- src/hooks/useRerollPlayers.js
+++ src/hooks/useRerollPlayers.js
@@ -1,4 +1,10 @@
+import { useState, useCallback } from "react";
+
+import { setFiveChampionsId } from "../setFiveInfo/setFiveIds";
+
 const useRerollPlayers = () => {
+  const [commanders, setCommanders] = useState([]);
+
   const getRandomPositions = (arrayLength, numOfPositions) => {
     let positions = [];
     let positionsAdded = 0;
@@ -12,7 +18,23 @@ const useRerollPlayers = () => {
     return positions;
   };
 
-  return {};
+  const getCommander = useCallback((numCommanders) => {
+    let randomCommanderPositions = getRandomPositions(
+      setFiveChampionsId.length,
+      numCommanders
+    );
+    randomCommanderPositions.map((randomCommander) => {
+      let commanderToPush = setFiveChampionsId[randomCommander];
+      return setCommanders((prevCommanders) => [
+        ...prevCommanders,
+        commanderToPush,
+      ]);
+    });
+  }, []);
+
+  return {
+    commanders,
+  };
 };
 
 export default useRerollPlayers;
