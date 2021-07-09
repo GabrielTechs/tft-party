43ebb74797a75ce2593ec07a61a5ffa680b3a07a
LAPTOP-K6QB57M2\gabha <gabha2107@gmail.com>
2021-07-09 15:46:47 -0400

    function that returns an array of random numbers for the champions and origins rendom positions

--- src/hooks/useRerollPlayers.js
+++ src/hooks/useRerollPlayers.js
@@ -1,4 +1,17 @@
 const useRerollPlayers = () => {
+  const getRandomPositions = (arrayLength, numOfPositions) => {
+    let positions = [];
+    let positionsAdded = 0;
+    while (positionsAdded < numOfPositions) {
+      let randomPosition = Math.floor(Math.random() * arrayLength);
+      if (positions.includes(randomPosition) === false) {
+        positions.push(randomPosition);
+        positionsAdded++;
+      }
+    }
+    return positions;
+  };
+
   return {};
 };
 
