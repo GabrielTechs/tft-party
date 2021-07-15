54599339bd9486a493606cf7833000e241063786
GabrielTechs <gabha2107@gmail.com>
2021-07-14 23:20:30 -0400

    displaying commanders, origins and teamSide given for the player card

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -19,18 +19,48 @@ const PlayerCard = (props) => {
           <h3 className="player-card-th">: {props.playerName}</h3>
         )}
       </PlayerCardNameDiv>
-      <h2>Commanders:</h2>
-      <ChampionCommander
-        championImg="https://rerollcdn.com/characters/Skin/5/Viego.png"
-        championName="Viego"
-      />
-      <ChampionCommander
-        championImg="https://rerollcdn.com/characters/Skin/5/Volibear.png"
-        championName="Volibear"
-      />
-      <h2>Origins:</h2>
-      <ChampionOrigin />
-      <ChampionOrigin />
+      {teamSide !== "none" && <h4>Team {teamSide}</h4>}
+      {commanders.length > 0 && (
+        <React.Fragment>
+          <h2>Commanders:</h2>
+          {commanders.map((champion, index) =>
+            props.champions.map((champ) => {
+              if (champ.championId === champion) {
+                return (
+                  <ChampionCommander
+                    key={index}
+                    championImg={champ.imgUrl}
+                    championName={champ.name}
+                    championCost={champ.cost}
+                  />
+                );
+              } else {
+                return false;
+              }
+            })
+          )}
+        </React.Fragment>
+      )}
+      {origins.length > 0 && (
+        <React.Fragment>
+          <h2>Origins:</h2>
+          {origins.map((originId, index) =>
+            props.traits.map((trait) => {
+              if (trait.key === originId) {
+                return (
+                  <ChampionOrigin
+                    key={index}
+                    origin={trait}
+                    champions={props.champions}
+                  />
+                );
+              } else {
+                return false;
+              }
+            })
+          )}
+        </React.Fragment>
+      )}
     </PlayerCardDiv>
   );
 };
