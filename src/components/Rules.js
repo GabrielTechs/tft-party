684cccdb4b02f5aebb1f81b6a9f97677db77a3de
GabrielTechs <gabha2107@gmail.com>
2021-07-17 15:10:01 -0400

    initializing Rules component

---
+++ src/components/Rules.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const Rules = () => {
+  return <RulesDiv></RulesDiv>;
+};
+
+const RulesDiv = styled.div``;
+
+export default Rules;
