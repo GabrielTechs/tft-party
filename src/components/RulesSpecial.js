2b7ea1af22167bd09e1447bedf9eeb12e0eeef74
GabrielTechs <gabha2107@gmail.com>
2021-07-19 15:58:22 -0400

    initializing RulesSpecial component

---
+++ src/components/RulesSpecial.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+const RulesSpecial = (props) => {
+  return <RulesSpecialDiv></RulesSpecialDiv>;
+};
+
+RulesSpecial.propTypes = {
+  specialRules: PropTypes.array,
+};
+
+const RulesSpecialDiv = styled.div``;
+
+export default RulesSpecial;
