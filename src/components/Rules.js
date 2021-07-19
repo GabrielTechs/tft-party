ae68546854246c30d09fad8efdf071bcdd489354
GabrielTechs <gabha2107@gmail.com>
2021-07-19 16:11:56 -0400

    import useRules to get modes and specialRules

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -1,7 +1,11 @@
 import React from "react";
 import styled from "styled-components";
 
+import useRules from "../hooks/useRules";
+
 const Rules = () => {
+  const { modes, specialRules, loadingRules } = useRules();
+
   return <RulesDiv></RulesDiv>;
 };
 
