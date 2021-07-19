5198e8325baf515936c50095d47d88a837a394c9
GabrielTechs <gabha2107@gmail.com>
2021-07-19 15:53:14 -0400

    initializing RulesModes component for RulesPage

---
+++ src/components/RulesModes.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const RulesModes = (props) => {
+  return <RulesModesDiv></RulesModesDiv>;
+};
+
+RulesModes.propTypes = {
+  mode: PropTypes.object,
+};
+
+const RulesModesDiv = styled.div``;
+
+export default RulesModes;
