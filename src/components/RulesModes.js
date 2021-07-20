7cc66992cfed3cc5862482d7e69d8bb782180fa7
GabrielTechs <gabha2107@gmail.com>
2021-07-20 14:26:03 -0400

    displaying the mode rules with props

--- src/components/RulesModes.js
+++ src/components/RulesModes.js
@@ -3,7 +3,16 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 
 const RulesModes = (props) => {
-  return <RulesModesDiv></RulesModesDiv>;
+  return (
+    <RulesModesDiv>
+      <h2>{props.mode.modeName}</h2>
+      <h3>{props.mode.modeDescription}</h3>
+      <h2>Mode rules:</h2>
+      {props.mode.rules.map((rule) => (
+        <h3 key={rule}>{rule}</h3>
+      ))}
+    </RulesModesDiv>
+  );
 };
 
 RulesModes.propTypes = {
