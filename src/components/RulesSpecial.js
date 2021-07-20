02e05918e5868f3adf0a3cd4d720e43b56602295
GabrielTechs <gabha2107@gmail.com>
2021-07-20 14:26:40 -0400

    displaying special rules with props

--- src/components/RulesSpecial.js
+++ src/components/RulesSpecial.js
@@ -3,7 +3,16 @@ import styled from "styled-components";
 import PropTypes from "prop-types";
 
 const RulesSpecial = (props) => {
-  return <RulesSpecialDiv></RulesSpecialDiv>;
+  return (
+    <RulesSpecialDiv>
+      {props.specialRules.map((specialRule) => (
+        <div key={specialRule.specialRuleName}>
+          <h2>{specialRule.specialRuleName}: </h2>
+          <h3>{specialRule.specialRule}</h3>
+        </div>
+      ))}
+    </RulesSpecialDiv>
+  );
 };
 
 RulesSpecial.propTypes = {
