887545da0f635f1374b7c3f8118515f731317010
GabrielTechs <gabha2107@gmail.com>
2021-07-20 14:24:52 -0400

    displaying the mode rules with props

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from "react";
 import styled from "styled-components";
+import { mediaQueries } from "../assets/mediaQueries";
 
 import useRules from "../hooks/useRules";
 
@@ -27,6 +28,20 @@ const Rules = () => {
   );
 };
 
-const RulesDiv = styled.div``;
+const RulesDiv = styled.div`
+  margin: 69px;
+  h1 {
+    text-align: center;
+    color: ${({ theme }) => theme.primaryText};
+    display: inline-block;
+    font-size: 2.69rem;
+    font-weight: 500;
+    margin: 9px;
+    width: 100%;
+  }
+  ${mediaQueries("md")`
+  margin: 26px;
+`}
+`;
 
 export default Rules;
