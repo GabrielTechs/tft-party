b1bb4b4e90b63432efc2082d1b76f8e15fe35d99
GabrielTechs <gabha2107@gmail.com>
2021-07-20 14:23:37 -0400

    importing RulesModes and RulesSpecial and displaying it in Rules component

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { Fragment } from "react";
 import styled from "styled-components";
 
 import useRules from "../hooks/useRules";
 
+import RulesModes from "./RulesModes";
+import RulesSpecial from "./RulesSpecial";
+
 const Rules = () => {
   const { modes, specialRules, loadingRules } = useRules();
 
-  return <RulesDiv></RulesDiv>;
+  return (
+    <RulesDiv>
+      {!loadingRules ? (
+        <Fragment>
+          <h1>Modes</h1>
+          {modes.map((mode) => (
+            <RulesModes key={mode.modeName} mode={mode} />
+          ))}
+          <h1>Special Rules</h1>
+          <RulesSpecial specialRules={specialRules} />
+        </Fragment>
+      ) : (
+        <h1>Loading...</h1>
+      )}
+    </RulesDiv>
+  );
 };
 
 const RulesDiv = styled.div``;
