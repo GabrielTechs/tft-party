bf7c5983359631a978b41fa35884d5a4ecf6be80
GabrielTechs <gabha2107@gmail.com>
2021-07-23 16:40:28 -0400

    styling RulesSpecialDiv

--- src/components/RulesSpecial.js
+++ src/components/RulesSpecial.js
@@ -19,6 +19,32 @@ RulesSpecial.propTypes = {
   specialRules: PropTypes.array,
 };
 
-const RulesSpecialDiv = styled.div``;
+const RulesSpecialDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  background: linear-gradient(
+    169deg,
+    ${({ theme }) => theme.primary} 36%,
+    ${({ theme }) => theme.secondary} 96%
+  );
+  border: 3px solid ${({ theme }) => theme.primary};
+  border-radius: 15px;
+  margin: 26px 0;
+  padding: 10px;
+  h2 {
+    color: ${({ theme }) => theme.secondaryText};
+    display: inline-block;
+    font-size: 1.8rem;
+    font-weight: 600;
+    margin: 6px;
+  }
+  h3 {
+    color: ${({ theme }) => theme.secondaryText};
+    display: inline-block;
+    font-size: 1.8rem;
+    font-weight: 500;
+    margin: 6px;
+  }
+`;
 
 export default RulesSpecial;
