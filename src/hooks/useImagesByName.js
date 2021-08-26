679957bc47c31a27eef0bd48094f6d4684a6f556
GabrielTechs <gabha2107@gmail.com>
2021-08-25 21:33:40 -0400

    getImage function to get the image requested by name

--- src/hooks/useImagesByName.js
+++ src/hooks/useImagesByName.js
@@ -1,10 +1,28 @@
 import { useState } from "react";
+import useFirestore from "./useFirestore";
 
 const useImagesByName = (imageName) => {
   const [image, setImage] = useState({});
   const [gettingImage, setGettingImage] = useState(true);
 
-  return { image, gettingImage };
+  const images = useFirestore("images", "name", "images");
+
+  const getImage = (imageName) => {
+    setGettingImage(true);
+    setImage({});
+    images.docsCategory.map((imagesDoc) => {
+      return imagesDoc.images.map((image) => {
+        if (image.name === imageName) {
+          setGettingImage(false);
+          return setImage(image);
+        } else {
+          return false;
+        }
+      });
+    });
+  };
+
+  return { images, image, gettingImage };
 };
 
 export default useImagesByName;
