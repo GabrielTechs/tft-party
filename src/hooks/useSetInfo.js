306a6d1a22ee04d27f02fb734530373235fe757d
GabrielTechs <gabha2107@gmail.com>
2021-08-26 21:36:26 -0400

    new traits state to store db traits request

--- src/hooks/useSetInfo.js
+++ src/hooks/useSetInfo.js
@@ -2,9 +2,11 @@ import { useState } from "react";
 
 const useSetInfo = () => {
   const [champions, setChampions] = useState([]);
+  const [traits, setTraits] = useState([]);
 
   return {
     champions,
+    traits,
   };
 };
 
