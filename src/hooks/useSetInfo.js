7aa4ae5f5c769e82f8be0a56fc18e74340982310
GabrielTechs <gabha2107@gmail.com>
2021-08-26 21:39:20 -0400

    new loadingSetInfo state to track db request state

--- src/hooks/useSetInfo.js
+++ src/hooks/useSetInfo.js
@@ -3,10 +3,12 @@ import { useState } from "react";
 const useSetInfo = () => {
   const [champions, setChampions] = useState([]);
   const [traits, setTraits] = useState([]);
+  const [loadingSetInfo, setLoadingSetInfo] = useState(true);
 
   return {
     champions,
     traits,
+    loadingSetInfo,
   };
 };
 
