c174f00a95d0e57eac9801edf5a9f651bcdda8f2
GabrielTechs <gabha2107@gmail.com>
2021-08-27 15:06:51 -0400

    useEffect to listen the db and setting the champions and traits states

--- src/hooks/useSetInfo.js
+++ src/hooks/useSetInfo.js
@@ -1,9 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+import { projectFirestore } from "../firebase/config";
 
 const useSetInfo = () => {
   const [champions, setChampions] = useState([]);
   const [traits, setTraits] = useState([]);
   const [loadingSetInfo, setLoadingSetInfo] = useState(true);
+
+  useEffect(() => {
+    let isMounted = true;
+    const collectionSetInfoRef = projectFirestore.collection("tftMidSet5Info");
+
+    const unsub = collectionSetInfoRef.onSnapshot((snap) => {
+      snap.forEach((doc) => {
+        if (doc.data().name === "champions") {
+          setChampions(doc.data().champions);
+        }
+        if (doc.data().name === "traits") {
+          setTraits(doc.data().traits);
+        }
+      });
+      if (isMounted) {
+        setLoadingSetInfo(false);
+      }
+    });
+    return () => {
+      isMounted = false;
+      unsub();
+    };
+  }, []);
 
   return {
     champions,
