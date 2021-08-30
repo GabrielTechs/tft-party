77abde290042d189f8358ee26b800f15dfaf5aa2
GabrielTechs <gabha2107@gmail.com>
2021-08-30 16:47:28 -0400

    refactoring champions and traits getters now using new custom hook useSetInfo

--- src/components/PlayersCards.js
+++ src/components/PlayersCards.js
@@ -6,13 +6,12 @@ import usePlayers from "../hooks/usePlayers";
 import PlayersNames from "./PlayersNames";
 import RerollPlayersSetup from "./RerollPlayersSetup";
 import PLayerCard from "./PlayerCard";
-import useFirestore from "../hooks/useFirestore";
+import useSetInfo from "../hooks/useSetInfo";
 
 const PlayersCards = () => {
   const [rerollSetup, setRerollSetup] = useState(false);
   const { playersInputs } = usePlayers();
-  const champions = useFirestore("tftSet5Info", "name", "champions");
-  const traits = useFirestore("tftSet5Info", "name", "traits");
+  const { champions, traits } = useSetInfo();
 
   const handleRerollSetup = () => {
     setRerollSetup((prevRerollSetup) => !prevRerollSetup);
@@ -24,19 +23,15 @@ const PlayersCards = () => {
       <PlayersNames players={playersInputs} />
       <RerollPlayersSetup handleRerollSetup={handleRerollSetup} />
       <h1>Players setup</h1>
-      {playersInputs.map((player) =>
-        champions.docsCategory.map((champ) =>
-          traits.docsCategory.map((trait) => (
-            <PLayerCard
-              key={`${rerollSetup} ${player.player}`}
-              player={player.player}
-              playerName={player.name}
-              champions={champ.champions}
-              traits={trait.traits}
-            />
-          ))
-        )
-      )}
+      {playersInputs.map((player) => (
+        <PLayerCard
+          key={`${rerollSetup} ${player.player}`}
+          player={player.player}
+          playerName={player.name}
+          champions={champions}
+          traits={traits}
+        />
+      ))}
     </PlayersCardsDiv>
   );
 };
