124c12d8a5884ce2a95483c931ed257082935b96
GabrielTechs <gabha2107@gmail.com>
2021-09-07 18:09:20 -0400

    initializing presentation component

---
+++ src/components/Presentation.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const Presentation = () => {
+  return <PresentationDiv></PresentationDiv>;
+};
+
+const PresentationDiv = styled.div``;
+
+export default Presentation;
