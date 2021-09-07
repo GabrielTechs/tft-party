1e496724f96e6e6780ea37567d545548e334b85c
GabrielTechs <gabha2107@gmail.com>
2021-09-07 18:10:25 -0400

    styling PresentationDiv

--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -5,6 +5,16 @@ const Presentation = () => {
   return <PresentationDiv></PresentationDiv>;
 };
 
-const PresentationDiv = styled.div``;
+const PresentationDiv = styled.div`
+  border-radius: 69px;
+  width: 70%;
+  background-size: cover;
+  overflow: hidden;
+  margin: auto;
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
 
 export default Presentation;
