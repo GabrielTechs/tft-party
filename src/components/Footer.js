9f48f7a6b218c1e081415bc19c2d2d9a2e9c0eba
GabrielTechs <gabha2107@gmail.com>
2021-09-09 20:22:21 -0400

    initializing Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const Footer = () => {
+  return <FooterDiv></FooterDiv>;
+};
+
+const FooterDiv = styled.div``;
+
+export default Footer;
