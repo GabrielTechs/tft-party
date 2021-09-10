1e123cbc104b6be223e654b47a98aaceb5ffdd37
GabrielTechs <gabha2107@gmail.com>
2021-09-09 20:25:06 -0400

    styling FooterDiv

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,6 +16,29 @@ const Footer = () => {
   );
 };
 
-const FooterDiv = styled.div``;
+const FooterDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  border-radius: 15px 15px 0 0;
+  background: linear-gradient(
+    169deg,
+    ${({ theme }) => theme.primary} 36%,
+    ${({ theme }) => theme.secondary} 96%
+  );
+  padding: 69px;
+  h1 {
+    color: ${({ theme }) => theme.secondaryText};
+    font-size: 1.69rem;
+    font-weight: 500;
+    text-align: center;
+  }
+  h2 {
+    color: ${({ theme }) => theme.secondaryText};
+    font-size: 1.36rem;
+    margin-top: 69px;
+    font-weight: 400;
+    text-align: center;
+  }
+`;
 
 export default Footer;
