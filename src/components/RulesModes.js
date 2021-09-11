91b14349fab292e18770a697372e07b695f03a04
GabrielTechs <gabha2107@gmail.com>
2021-09-11 14:15:55 -0400

    verifying if the mode exist to avoid errors

--- src/components/RulesModes.js
+++ src/components/RulesModes.js
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { Fragment } from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
 const RulesModes = (props) => {
   return (
     <RulesModesDiv>
-      <h2>{props.mode.modeName}</h2>
-      <h3>{props.mode.modeDescription}</h3>
-      <h2>Mode rules:</h2>
-      {props.mode.rules.map((rule) => (
-        <h3 key={rule}>{rule}</h3>
-      ))}
+      {props.mode && (
+        <Fragment>
+          <h2>{props.mode.modeName}</h2>
+          <h3>{props.mode.modeDescription}</h3>
+          <h2>Mode rules:</h2>
+          {props.mode.rules.map((rule) => (
+            <h3 key={rule}>{rule}</h3>
+          ))}
+        </Fragment>
+      )}
     </RulesModesDiv>
   );
 };
