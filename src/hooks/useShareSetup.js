7809f60f35efdc314f21b6aabe13242331abeb3e
GabrielTechs <gabha2107@gmail.com>
2021-09-13 16:09:43 -0400

    creating an initial state to playersSetup to avoid conditioning errors

--- src/hooks/useShareSetup.js
+++ src/hooks/useShareSetup.js
@@ -1,7 +1,18 @@
 import { useState } from "react";
 
+const playersSetupInitialState = [
+  { player: "Player 1" },
+  { player: "Player 2" },
+  { player: "Player 3" },
+  { player: "Player 4" },
+  { player: "Player 5" },
+  { player: "Player 6" },
+  { player: "Player 7" },
+  { player: "Player 8" },
+];
+
 const useShareSetup = () => {
-  const [playersSetup, setPlayerSetup] = useState();
+  const [playersSetup, setPlayerSetup] = useState(playersSetupInitialState);
   const [idToShare, setIdToShare] = useState("");
 
   return { playersSetup, idToShare };
