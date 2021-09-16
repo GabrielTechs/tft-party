d995f4dca51c836863869dcfefa52ad8c3c9f2ed
GabrielTechs <gabha2107@gmail.com>
2021-09-16 16:45:56 -0400

    importing Presentation component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from "react";
 
+import Presentation from "../components/Presentation";
 import ModeHandler from "../components/ModeHandler";
 import SpecialRules from "../components/SpecialRules";
 import RulesActive from "../components/RulesActive";
