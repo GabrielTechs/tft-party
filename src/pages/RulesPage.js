3243d1419b92e2177549cb3a530a0b88d66643d5
GabrielTechs <gabha2107@gmail.com>
2021-09-16 16:48:19 -0400

    importing and displaying Footer component in page

--- src/pages/RulesPage.js
+++ src/pages/RulesPage.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from "react";
 
 import Rules from "../components/Rules";
+import Footer from "../components/Footer";
 
 const RulesPage = () => {
   return (
     <Fragment>
       <Rules />
+      <Footer />
     </Fragment>
   );
 };
