2785403eb0876054d556d0ab7f1f68dbb959ecb7
GabrielTechs <gabha2107@gmail.com>
2021-09-21 13:14:35 -0400

    initializing ShareSetup component

---
+++ src/components/ShareSetup.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const ShareSetup = (props) => {
+  return <ShareSetupDiv></ShareSetupDiv>;
+};
+
+const ShareSetupDiv = styled.div``;
+
+export default ShareSetup;
