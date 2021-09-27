4c521f42692adb0d5411d499a9fdeba0cb633d33
GabrielTechs <gabha2107@gmail.com>
2021-09-27 16:00:54 -0400

    adding shareSetup prop type

--- src/components/SaveSetupBtn.js
+++ src/components/SaveSetupBtn.js
@@ -1,8 +1,13 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styled from "styled-components";
 
 const SaveSetupBtn = (props) => {
   return <SBtnSaveSetup></SBtnSaveSetup>;
+};
+
+SaveSetupBtn.propTypes = {
+  shareSetup: PropTypes.func,
 };
 
 const SBtnSaveSetup = styled.button``;
