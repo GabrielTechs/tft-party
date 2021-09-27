385cfc2fd3bb65a64edce6fc3131dea8cb1c8df6
GabrielTechs <gabha2107@gmail.com>
2021-09-27 16:01:51 -0400

    adding shareSetup prop type

--- src/components/ShareSetup.js
+++ src/components/ShareSetup.js
@@ -1,8 +1,13 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styled from "styled-components";
 
 const ShareSetup = (props) => {
   return <ShareSetupDiv></ShareSetupDiv>;
+};
+
+ShareSetup.propTypes = {
+  shareSetup: PropTypes.func,
 };
 
 const ShareSetupDiv = styled.div``;
