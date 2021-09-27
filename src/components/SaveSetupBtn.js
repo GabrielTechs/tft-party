36928e86c3597467507ff7f56a952ebb6540ee03
GabrielTechs <gabha2107@gmail.com>
2021-09-27 16:00:03 -0400

    initializing new component SaveSetupBtn

---
+++ src/components/SaveSetupBtn.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const SaveSetupBtn = (props) => {
+  return <SBtnSaveSetup></SBtnSaveSetup>;
+};
+
+const SBtnSaveSetup = styled.button``;
+
+export default SaveSetupBtn;
