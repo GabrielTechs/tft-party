3e9048a0773eb040face3d30ef3d316e5bde0379
GabrielTechs <gabha2107@gmail.com>
2021-09-28 14:38:13 -0400

    initialize SavedSetupLink component

---
+++ src/components/SavedSetupLink.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const SavedSetupLink = (props) => {
+  return <SavedSetupLinkText></SavedSetupLinkText>;
+};
+
+const SavedSetupLinkText = styled.p``;
+
+export default SavedSetupLink;
