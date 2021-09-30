ac3afce6d9aa7f058095db80044996ede0dd2b1f
GabrielTechs <gabha2107@gmail.com>
2021-09-30 16:45:28 -0400

    getting idToShare as string propType

--- src/components/SavedSetupLink.js
+++ src/components/SavedSetupLink.js
@@ -1,8 +1,13 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styled from "styled-components";
 
 const SavedSetupLink = (props) => {
   return <SavedSetupLinkText></SavedSetupLinkText>;
+};
+
+SavedSetupLink.propTypes = {
+  idToShare: PropTypes.string,
 };
 
 const SavedSetupLinkText = styled.p``;
