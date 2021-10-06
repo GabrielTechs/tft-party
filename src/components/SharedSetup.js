b7313ccb7bb81d421b2c31de06f1ef98cb8bb1d0
GabrielTechs <gabha2107@gmail.com>
2021-10-06 15:57:14 -0400

    setupId prop type added

--- src/components/SharedSetup.js
+++ src/components/SharedSetup.js
@@ -1,8 +1,13 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styled from "styled-components";
 
 const SharedSetup = (props) => {
   return <SharedSetupDiv></SharedSetupDiv>;
+};
+
+SharedSetup.propTypes = {
+  setupId: PropTypes.string,
 };
 
 const SharedSetupDiv = styled.div``;
