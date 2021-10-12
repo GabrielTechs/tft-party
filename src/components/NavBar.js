4d7477a70836e98caf436199aeabf5b04e6777a0
GabrielTechs <gabha2107@gmail.com>
2021-10-12 15:31:15 -0400

    imported useImagesByName custom hook into NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
+import useImagesByName from "../hooks/useImagesByName";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import ThemeToggleBtn from "./ThemeToggleBtn";
 
