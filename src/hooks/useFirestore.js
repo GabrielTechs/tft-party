1d80864194235e6fa898b4c26f1d617d7db53c6e
GabrielTechs <gabha2107@gmail.com>
2022-02-24 13:40:26 -0400

    reformated firebase code due to v9 updates

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -1,28 +1,20 @@
 import { useState, useEffect } from "react";
 import { projectFirestore } from "../firebase/config";
+import { collection, onSnapshot, query, where } from "@firebase/firestore";
 
-const useFirestore = (collection, category, categoryName) => {
+const useFirestore = (collectionName, category, categoryName) => {
   const [docsCategory, setDocsCategory] = useState([]);
 
   useEffect(() => {
-    const collectionRef = projectFirestore.collection(collection);
+    const collectionRef = collection(projectFirestore, collectionName);
 
     // Listening to the collection filtered by category.
     if (category !== "all") {
-      const unsub = collectionRef
-        .where(category, "==", categoryName)
-        .onSnapshot((snap) => {
-          let categoryDocuments = [];
-          snap.forEach((doc) => {
-            categoryDocuments.push({ ...doc.data(), id: doc.id });
-          });
-          setDocsCategory(categoryDocuments);
-        });
-      return () => unsub();
-    }
-    // Listening to the entire collection.
-    else {
-      const unsub = collectionRef.onSnapshot((snap) => {
+      const queryCategoryName = query(
+        collectionRef,
+        where(category, "==", categoryName)
+      );
+      const unsub = onSnapshot(queryCategoryName, (snap) => {
         let categoryDocuments = [];
         snap.forEach((doc) => {
           categoryDocuments.push({ ...doc.data(), id: doc.id });
@@ -31,7 +23,18 @@ const useFirestore = (collection, category, categoryName) => {
       });
       return () => unsub();
     }
-  }, [collection, category, categoryName]);
+    // Listening to the entire collection.
+    else {
+      const unsub = onSnapshot(collectionRef, (snap) => {
+        let categoryDocuments = [];
+        snap.forEach((doc) => {
+          categoryDocuments.push({ ...doc.data(), id: doc.id });
+        });
+        setDocsCategory(categoryDocuments);
+      });
+      return () => unsub();
+    }
+  }, [collectionName, category, categoryName]);
 
   return { docsCategory };
 };
