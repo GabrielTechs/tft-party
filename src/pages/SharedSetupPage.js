cd892af18099eac48c7fb51eb4575a82db5ca45b
GabrielTechs <gabha2107@gmail.com>
2022-04-25 19:08:44 -0400

    changed params getter

--- src/pages/SharedSetupPage.js
+++ src/pages/SharedSetupPage.js
@@ -1,24 +1,17 @@
 import React, { Fragment } from "react";
-import PropTypes from "prop-types";
 
 import SharedSetup from "../components/SharedSetup";
+import { useParams } from "react-router-dom";
 
-const SharedSetupPage = (props) => {
-  const setupId = props.match.params.setupId;
+const SharedSetupPage = () => {
+  const params = useParams();
+  const setupId = params.setupId;
 
   return (
     <Fragment>
       <SharedSetup setupId={setupId} />
     </Fragment>
   );
-};
-
-SharedSetupPage.propTypes = {
-  match: PropTypes.shape({
-    params: PropTypes.shape({
-      setupId: PropTypes.string,
-    }),
-  }),
 };
 
 export default SharedSetupPage;
