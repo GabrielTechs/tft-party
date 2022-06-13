a700f0113c84b766253f6cff050dc5c469fa3fd1
GabrielTechs <gabha2107@gmail.com>
2022-06-13 16:00:38 -0400

    added meta tags with og property

--- src/assets/HelmetData.js
+++ src/assets/HelmetData.js
@@ -18,6 +18,20 @@ const HelmetData = (props) => {
         name="twitter:image"
         content="https://firebasestorage.googleapis.com/v0/b/tftparty-bde27.appspot.com/o/images%2Ftftparty-vertical-wb.png?alt=media&token=9da5a2e7-f94a-4aab-80a5-9c8c2335a2f7"
       />
+      <meta property="og:title" content="Gabriel Alguacil" />
+      <meta
+        property="og:description"
+        content="Software engineer passionate about front end development and new tech."
+      />
+      <meta
+        property="og:image"
+        content="https://firebasestorage.googleapis.com/v0/b/tftparty-bde27.appspot.com/o/images%2Ftftparty-vertical-wb.png?alt=media&token=9da5a2e7-f94a-4aab-80a5-9c8c2335a2f7"
+      />
+      <meta property="og:url" content="tftparty.com" />
+      <meta property="og:site_name" content="Tftparty" />
+      <meta property="og:locale" content="es_ES" />
+      <meta property="og:type" content="article" />
+      <meta property="fb:app_id" content="ID_APP_FACEBOOK" />
     </Helmet>
   );
 };
