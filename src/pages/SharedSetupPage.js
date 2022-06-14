b51fbcb4074cc133d972ba6cbd842659ec381db9
GabrielTechs <gabha2107@gmail.com>
2022-06-14 13:19:36 -0400

    imported and added HelmetData with title and description

--- src/pages/SharedSetupPage.js
+++ src/pages/SharedSetupPage.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from "react";
+import HelmetData from "../assets/HelmetData";
 
 import SharedSetup from "../components/SharedSetup";
 import { useParams } from "react-router-dom";
@@ -9,6 +10,10 @@ const SharedSetupPage = () => {
 
   return (
     <Fragment>
+      <HelmetData
+        title="Tftparty - shared setup"
+        description="Multiple game modes and easy to share to play tft with friends and have a different expirience"
+      />
       <SharedSetup setupId={setupId} />
     </Fragment>
   );
