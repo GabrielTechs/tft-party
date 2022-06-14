3786faf821ef3b16442964430422f97ff4fe31e5
GabrielTechs <gabha2107@gmail.com>
2022-06-14 13:19:29 -0400

    imported and added HelmetData with title and description

--- src/pages/RulesPage.js
+++ src/pages/RulesPage.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from "react";
+import HelmetData from "../assets/HelmetData";
 
 import Rules from "../components/Rules";
 import Footer from "../components/Footer";
@@ -6,6 +7,10 @@ import Footer from "../components/Footer";
 const RulesPage = () => {
   return (
     <Fragment>
+      <HelmetData
+        title="Tftparty - all modes rules"
+        description="Multiple game modes and easy to share to play tft with friends and have a different expirience"
+      />
       <Rules />
       <Footer />
     </Fragment>
