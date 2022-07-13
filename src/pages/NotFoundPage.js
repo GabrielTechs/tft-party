d9398fe0ff685dbb3522fd47cea8ad6e01852764
GabrielTechs <gabha2107@gmail.com>
2022-07-13 16:00:24 -0400

    created a new not found page

---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFoundPage = () => {
+  return (
+    <React.Fragment>
+      <h1>Not found</h1>
+    </React.Fragment>
+  );
+};
+
+export default NotFoundPage;
